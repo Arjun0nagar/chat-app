fa0c1c34156ef4bae96b7e1c86ab84d8b72ebf60
GURJAR <gurjar@GURJARs-MacBook-Air.local>
2025-10-04 19:00:49 +0530

    Initial commit

---
+++ server/controller/userController.js
@@ -0,0 +1,92 @@
+import { generateToken } from "../library/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import cloudinary from "../library/clodinary.js";
+
+// Sign-up
+export const signUp = async (req,res)=>{
+
+    const {fullName,email,password,bio}=req.body;
+
+    try {
+        if(!fullName || !email || !password || !bio){
+            return res.json({success : false,message:"Missing Details"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.json({success : false,message:"User already exists for this email"});
+        }
+
+        const salt= await bcrypt.genSalt(10);
+        const hashedpass= await bcrypt.hash(password,salt);
+
+        const newUser= await User.create({
+            fullName,email,password:hashedpass,bio
+        });
+
+        const token = generateToken(newUser._id);
+        res.json({success: true , userData:newUser, token ,message : "Account succesfully created"});
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success : false,message:error.message});
+    }
+}
+
+// Login
+export const login = async (req,res)=>{
+
+    const {email,password}=req.body;
+
+    try {
+        if(!email || !password ){
+            return res.json({success : false,message:"Missing Details"});
+        }
+        const userData = await User.findOne({email});
+        if(!userData){
+            res.json({success:false,messgae:"User doesnot exist for this email"});
+        }
+        const isPassCorrect = bcrypt.compare(password,userData.password)
+
+        if(!isPassCorrect){
+            return res.json({success : false,message:"Invalid Details"});
+        }
+
+        const token = generateToken(userData._id)
+        res.json({success: true , userData, token ,message : "Login succesfull"});
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success : false , message:error.message});
+    }
+}
+
+// To check if user is authenticated
+export const checkAuth = (req,res)=>{
+    res.json({success:true,user:req.user});
+}
+
+// To update user profile
+export const updateProfile = async (req,res)=>{
+
+    try {
+        const { profilePic,bio,fullName }=req.body;
+        const userId=req.user._id;
+
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId,{bio,fullName},{new:true}); 
+        }
+        else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser = await User.findByIdAndUpdate(userId,{profilePic:upload.secure_url ,bio,fullName},{new:true}); 
+        }
+        res.json({success:true,user:updatedUser});
+    } catch (error) {
+        res.json({success:false,message:error.message});
+        
+    }
+}
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {signUp,login, updateProfile, checkAuth} from "../controller/userController.js"
+import {protectRoutes} from "../middleware/auth.js"
+
+const userRouter = express.Router();
+
+userRouter.post("/signup",signUp);
+userRouter.post("/login",login);
+userRouter.put("/update-profile",protectRoutes,updateProfile);
+userRouter.get("/check",protectRoutes,checkAuth);
+
+export default userRouter;
